29af88d0f10bc134e14cf16b32b68762ce35d8fa
Hwang Dong Hyun <hwang.d.ab@m.titech.ac.jp>
2017-05-12 21:09:23 +0900

    Add multi sensing functions.

--- app/src/main/java/jp/ac/titech/itpro/sdl/accelgraph/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/accelgraph/MainActivity.java
@@ -21,7 +21,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     private GraphView xView, yView, zView;
 
     private SensorManager sensorMgr;
-    private Sensor accelerometer;
+
+
+    private Sensor proximitySonsor;
+    private Sensor lightSensor;
+    private Sensor gravitySensor;
 
     private final static long GRAPH_REFRESH_WAIT_MS = 20;
 
@@ -29,6 +33,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Handler handler;
 
     private float vx, vy, vz;
+
+    private float lightVal, gravityVal, proxiVal;
     private float rate;
     private int accuracy;
     private long prevts;
@@ -48,13 +54,37 @@ public class MainActivity extends Activity implements SensorEventListener {
         zView = (GraphView) findViewById(R.id.z_view);
 
         sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer == null) {
-            Toast.makeText(this, getString(R.string.toast_no_accel_error),
+
+        lightSensor = sensorMgr.getDefaultSensor(Sensor.TYPE_LIGHT);
+        gravitySensor = sensorMgr.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        proximitySonsor = sensorMgr.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+
+
+        if(lightSensor == null)
+        {
+            Toast.makeText(this, getString(R.string.toast_no_light_error),
                     Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
+
+        if(gravitySensor == null)
+        {
+            Toast.makeText(this, getString(R.string.toast_no_gravity_error),
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        if(proximitySonsor == null)
+        {
+            Toast.makeText(this, getString(R.string.toast_no_proximity_error),
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+
 
         handler = new Handler();
     }
@@ -63,7 +93,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "onResume");
-        sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+        sensorMgr.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorMgr.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorMgr.registerListener(this, proximitySonsor, SensorManager.SENSOR_DELAY_FASTEST);
         th = new GraphRefreshThread();
         th.start();
     }
@@ -78,9 +111,22 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        vx = alpha * vx + (1 - alpha) * event.values[0];
-        vy = alpha * vy + (1 - alpha) * event.values[1];
-        vz = alpha * vz + (1 - alpha) * event.values[2];
+
+    if(event.sensor.getType() == Sensor.TYPE_PROXIMITY)
+    {
+        proxiVal = event.values[0];
+    }
+    else if (event.sensor.getType() == Sensor.TYPE_LIGHT)
+    {
+        lightVal = alpha * lightVal + (1-alpha)*event.values[0];
+        lightVal /= 6;
+        lightVal *= -1;
+
+    }
+    else if (event.sensor.getType() == Sensor.TYPE_GRAVITY)
+    {
+        gravityVal = alpha * gravityVal + (1-alpha)*event.values[0];
+    }
         rate = ((float) (event.timestamp - prevts)) / (1000 * 1000);
         prevts = event.timestamp;
     }
@@ -99,9 +145,9 @@ public class MainActivity extends Activity implements SensorEventListener {
                         public void run() {
                             rateView.setText(String.format(Locale.getDefault(), "%f", rate));
                             accuracyView.setText(String.format(Locale.getDefault(), "%d", accuracy));
-                            xView.addData(vx, true);
-                            yView.addData(vy, true);
-                            zView.addData(vz, true);
+                            xView.addData(proxiVal, true);
+                            yView.addData(lightVal, true);
+                            zView.addData(gravityVal, true);
                         }
                     });
                     Thread.sleep(GRAPH_REFRESH_WAIT_MS);
